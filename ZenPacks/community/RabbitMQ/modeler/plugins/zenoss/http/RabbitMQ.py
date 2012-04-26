3053d43de8cb16514d95e09b0fe432e908068e47
Jason Harvey <alienth@reddit.com>
2012-04-26 02:10:35 -0700

    update license header

--- ZenPacks/community/RabbitMQ/modeler/plugins/zenoss/http/RabbitMQ.py
+++ ZenPacks/community/RabbitMQ/modeler/plugins/zenoss/http/RabbitMQ.py
@@ -1,13 +1,11 @@
 ###########################################################################
 #
-# This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2011, Zenoss Inc.
+# Copyright (C) 2012, Jason Harvey
+# Based on code by Zenoss, Inc. -- Copyright (C) 2011, Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 or (at your
 # option) any later version as published by the Free Software Foundation.
-#
-# For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
 
